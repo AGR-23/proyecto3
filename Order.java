4c10fb72772a5f97378d4a5966256ea2b156db36
agr <agarcesr@eafit.edu.co>
2025-05-09 14:15:00 -0500

    Adding delay

--- Order.java
+++ Order.java
@@ -4,53 +4,63 @@ import java.awt.Color;
 
 public class Order implements Directions {
 
-    // Trains for Line A and Line B
     public ArrayList<Train> lineATrains = new ArrayList<>();
     public ArrayList<Train> lineBTrains = new ArrayList<>();
-    public int[][] map = new int[36][21]; 
+    public int[][] map = new int[36][21];
 
     public Order() {
-        int columnPos    = 15;   // initial avenue
-        int rowPos       = 34;   // initial street
+        int columnPos = 15;
+        int rowPos    = 34;
         Direction currentDirection = Directions.North;
-        int counter      = 0;    
 
-        // Create trains in depot, alternating routes
-        while (lineATrains.size() < 22 || lineBTrains.size() < 10) {
-
-            if (counter % 3 == 0) 
-            {
-                Train trainB = new Train(rowPos, columnPos, currentDirection, 0, Color.GREEN, "routeSJ", this);
-                lineBTrains.add(trainB);
-            }
-            else if (counter % 3 == 1) 
-            {
-                Train trainA = new Train(rowPos, columnPos, currentDirection, 0, Color.BLUE, "routeAN", this);
-                lineATrains.add(trainA);
-            }
-            else 
-            {
-                Train trainA = new Train(rowPos, columnPos, currentDirection, 0, Color.BLUE, "routeAE", this);
-                lineATrains.add(trainA);
-            }
-
-            // mark this cell occupied
+        // 1) Crear 24 trenes de la línea A
+        for (int i = 0; i < 24; i++) {
+            String routeA = (i % 2 == 0) ? "routeAN" : "routeAE";
+            Train trainA = new Train(rowPos, columnPos, currentDirection, 0, Color.BLUE, routeA, this);
+            lineATrains.add(trainA);
             map[rowPos][columnPos] = 1;
 
-            // move starting position in the current direction
-            if (currentDirection == Directions.North)      rowPos++;
-            else if (currentDirection == Directions.South) rowPos--;
-            else if (currentDirection == Directions.East)  columnPos++;
-            else if (currentDirection == Directions.West)  columnPos--;
-
-            // turn at corners
-            if (columnPos == 15 && rowPos == 35) currentDirection = Directions.West;    
-            if (columnPos == 1  && rowPos == 35) currentDirection = Directions.South;  
-            if (columnPos == 1  && rowPos == 34) currentDirection = Directions.East;  
-            if (columnPos == 14 && rowPos == 34) currentDirection = Directions.South;  
-            if (columnPos == 14 && rowPos == 32) currentDirection = Directions.East;  
-
-            counter++;
+            Position next = advanceAndTurn(rowPos, columnPos, currentDirection);
+            rowPos = next.row;
+            columnPos = next.col;
+            currentDirection = next.dir;
         }
+
+        // 2) Crear 8 trenes de la línea B
+        for (int j = 0; j < 8; j++) {
+            Train trainB = new Train(rowPos, columnPos, currentDirection, 0, Color.GREEN, "routeSJ", this);
+            lineBTrains.add(trainB);
+            map[rowPos][columnPos] = 1;
+
+            Position next = advanceAndTurn(rowPos, columnPos, currentDirection);
+            rowPos = next.row;
+            columnPos = next.col;
+            currentDirection = next.dir;
+        }
+    }
+
+    private static class Position {
+        int row, col;
+        Direction dir;
+        Position(int row, int col, Direction dir) {
+            this.row = row; this.col = col; this.dir = dir;
+        }
+    }
+
+    private Position advanceAndTurn(int row, int col, Direction dir) {
+        // mover según la dirección
+        if (dir == Directions.North)      row++;
+        else if (dir == Directions.South) row--;
+        else if (dir == Directions.East)  col++;
+        else if (dir == Directions.West)  col--;
+
+        // giros en las esquinas
+        if (col == 15 && row == 35)        dir = Directions.West;
+        else if (col == 1  && row == 35)   dir = Directions.South;
+        else if (col == 1  && row == 34)   dir = Directions.East;
+        else if (col == 14 && row == 34)   dir = Directions.South;
+        else if (col == 14 && row == 32)   dir = Directions.East;
+
+        return new Position(row, col, dir);
     }
 }
--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) throws InterruptedException {
         World.readWorld("MetroMed.kwld");
         World.setVisible(true);
-        World.setDelay(1);   // ralentiza para ver el movimiento
+        World.setDelay(7);   // ralentiza para ver el movimiento
 
         Order order = new Order();
         List<Train> lineA = order.lineATrains;
@@ -18,20 +18,22 @@ public class main {
         // mientras queden trenes en A o en B
         while (idxA >= 0 || idxB >= 0) {
             // Batch: 2 trenes de A
-            for (int i = 0; i < 12 && idxA >= 0; i++, idxA--) {
+            for (int i = 0; i < 2 && idxA >= 0; i++, idxA--) {
                 Train t = lineA.get(idxA);
                 System.out.println("Arrancando A row="+t.row);
                 new Thread(t).start();
             }
             // Batch: 1 tren de B
-            if (idxB >= -6) {
+            if (idxB >= 0) {
                 Train t = lineB.get(idxB);
                 System.out.println("Arrancando B row="+t.row);
                 new Thread(t).start();
                 idxB--;
             }
             // Espera 15 s antes del siguiente batch (si aún quedan trenes)
-              
+            if (idxA >= 0 || idxB >= 0) {
+                Thread.sleep(15_00);
+            }
         }
     }
 }
